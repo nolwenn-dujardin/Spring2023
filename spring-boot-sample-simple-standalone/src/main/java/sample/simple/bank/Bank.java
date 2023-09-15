3745288f39c54e663be2bdab49ace78415b6c9b3
Jean-Baptiste BOUGAUD <jbaptiste.bougaud@gmail.com>
2023-09-15 12:45:47 +0200

    add sout (not in Client)

--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/bank/Bank.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/bank/Bank.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Component;
 public class Bank implements IBank{
     @Override
     public void transfert() {
-
+        System.out.println("The transfer is beginning.");
     }
 }
--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/provider/Provider.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/provider/Provider.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Component;
 public class Provider implements IProvider{
     @Override
     public int getPrice() {
+        System.out.println("The product costs 5€");
         return 0;
     }
 
     @Override
     public void order() {
-
+        System.out.println("Order begins...");
+        System.out.println();
+        System.out.println("Order ends");
     }
 }
