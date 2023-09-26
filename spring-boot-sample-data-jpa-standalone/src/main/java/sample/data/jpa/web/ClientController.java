b37126653f0076b26c981303bfad6c41c1eac36c
Nolwenn DUJARDIN <nolwenn35560@gmail.com>
2023-09-26 16:50:02 +0200

    Modification Client et Professional Controller

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/ClientController.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/ClientController.java
@@ -1,4 +1,51 @@
 package sample.data.jpa.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import sample.data.jpa.domain.Client;
+import sample.data.jpa.service.ClientDAO;
+import sample.data.jpa.service.UserDao;
+
+@Controller
 public class ClientController {
+
+    @Autowired
+    private ClientDAO clientDao;
+
+    /**
+     * POST /create  --> Create a new user and save it in the database.
+     */
+    @RequestMapping(value = "/create-client", method = RequestMethod.POST)
+    @ResponseBody
+    public String create(@RequestBody Client client) {
+        String userId = "";
+        try {
+            clientDao.save(client);
+            userId = String.valueOf(client.getId());
+        }
+        catch (Exception ex) {
+            return "Error creating the user: " + ex.toString();
+        }
+        return "User succesfully created with id = " + userId;
+    }
+
+    /**
+     * PUT /update  --> Update the email and the name for the user in the
+     * database having the passed id.
+     */
+    @RequestMapping(value = "/update-client", method = RequestMethod.PUT)
+    @ResponseBody
+    public String updateUser(@RequestParam long id, @RequestParam String email, @RequestParam String name) {
+        try {
+            Client client = clientDao.findById(id).get();
+            client.setEmail(email);
+            client.setName(name);
+            clientDao.save(client);
+        }
+        catch (Exception ex) {
+            return "Error updating the user: " + ex.toString();
+        }
+        return "User succesfully updated!";
+    }
 }
--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/ProfessionalController.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/ProfessionalController.java
@@ -1,4 +1,49 @@
 package sample.data.jpa.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import sample.data.jpa.domain.Professional;
+import sample.data.jpa.service.ProfessionalDAO;
+
+@Controller
 public class ProfessionalController {
+    @Autowired
+    private ProfessionalDAO proDao;
+
+    /**
+     * POST /create  --> Create a new user and save it in the database.
+     */
+    @RequestMapping(value = "/create-pro", method = RequestMethod.POST)
+    @ResponseBody
+    public String create(@RequestBody Professional pro) {
+        String userId = "";
+        try {
+            proDao.save(pro);
+            userId = String.valueOf(pro.getId());
+        }
+        catch (Exception ex) {
+            return "Error creating the user: " + ex.toString();
+        }
+        return "User succesfully created with id = " + userId;
+    }
+
+    /**
+     * PUT /update  --> Update the email and the name for the user in the
+     * database having the passed id.
+     */
+    @RequestMapping(value = "/update-pro", method = RequestMethod.PUT)
+    @ResponseBody
+    public String updateUser(@RequestParam long id, @RequestParam String email, @RequestParam String name) {
+        try {
+            Professional pro = proDao.findById(id).get();
+            pro.setEmail(email);
+            pro.setName(name);
+            proDao.save(pro);
+        }
+        catch (Exception ex) {
+            return "Error updating the user: " + ex.toString();
+        }
+        return "User succesfully updated!";
+    }
 }
