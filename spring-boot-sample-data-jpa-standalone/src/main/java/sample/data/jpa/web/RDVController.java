5c0db7e9b7d4b8d9785e379a3294138bd49f5827
Jean-Baptiste BOUGAUD <jbaptiste.bougaud@gmail.com>
2023-09-26 17:02:27 +0200

    add rdv controller

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/RDVController.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/RDVController.java
@@ -1,4 +1,72 @@
 package sample.data.jpa.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import sample.data.jpa.domain.RDV;
+import sample.data.jpa.domain.User;
+import sample.data.jpa.service.RDVDAO;
+
+@Controller
 public class RDVController {
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String create(@RequestBody RDV rdv) {
+        String userId = "";
+        try {
+            rdvdao.save(rdv);
+            userId = String.valueOf(rdv.getId());
+        }
+        catch (Exception ex) {
+            return "Error creating the RDV: " + ex.toString();
+        }
+        return "RDV succesfully created with id = " + userId;
+    }
+
+    @RequestMapping(value = "/rdv/{RdvId}", method = RequestMethod.GET)
+    @ResponseBody
+    public String get(@PathVariable("RdvId") Long id) {
+        try {
+            RDV r = rdvdao.getReferenceById(id);
+            return r.getName();
+        }
+        catch (Exception ex) {
+            return "Error creating the RDV: " + ex.toString();
+        }
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    @ResponseBody
+    public String create(@RequestBody Long id) {
+        try {
+            rdvdao.deleteById(id);
+        }
+        catch (Exception ex) {
+            return "Error creating the user: " + ex.toString();
+        }
+        return "RDV succesfully deleted";
+    }
+
+    @RequestMapping(value = "/update/{RdvId}", method = RequestMethod.PUT)
+    @ResponseBody
+    public String update(@RequestBody RDV rdv, @PathVariable("RdvId") Long rdvId) {
+        String userId = "";
+        try {
+            RDV r = rdvdao.getReferenceById(rdvId);
+            r.setName(rdv.getName());
+            r.setClient(rdv.getClient());
+            r.setProfessional(rdv.getProfessional());
+            r.setReserved(rdv.isReserved());
+            r.setStart_time(rdv.getStart_time());
+            rdvdao.save(r);
+        }
+        catch (Exception ex) {
+            return "Error updating the RDV: " + ex.toString();
+        }
+        return "RDV succesfully updated with id = " + rdvId;
+    }
+
+    @Autowired
+    RDVDAO rdvdao;
 }
